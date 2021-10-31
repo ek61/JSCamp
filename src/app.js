ff040201f176f76aa8cc73bc3ffbe6f5aff28bbc
Emir ÇELİK <celik.emircelik@gmail.com>
2021-10-31 16:57:45 +0300

    31/10/2021

--- src/app.js
+++ src/app.js
@@ -29,3 +29,57 @@ console.log("</ul>")
 
 
 console.log(konutKredileri)
+
+let student = {id:1, name:"Emir"}
+console.log(student)
+
+function save(puan=10,ogrenci) {
+    console.log(ogrenci.name + " : " + puan)
+}
+save(undefined,student)
+
+let students = ["Engin DEMİROĞ","Emir ÇELİK","Berke ASMAKAYA","Halit KALAYCI"]
+
+console.log(students)
+
+let students2 = [student,{id:2, name:"Emir"},"Trabzon",{city:"İstanbul"}]
+console.log(students2)
+
+//Rest
+//params
+let showProduct = function (id,...products) {
+    console.log(id)
+    console.log(products)
+}
+
+//console.log(typeof showProduct) => function
+showProduct(10,"Elma","Armut","Karpuz")
+
+//Spread
+let points = [1,2,3,4,50,4,60,14]
+console.log(...points)
+console.log(Math.max(...points))
+console.log(..."ABC","D",..."EFG","H")
+
+//Destructuring
+let populations = [10000,20000,30000,[40000,100000]]
+let [small,medium,high,[veryHigh,maximum]] = populations
+console.log(small)
+console.log(medium)
+console.log(high)
+console.log(veryHigh)
+console.log(maximum)
+
+function someFunction([small1],number) {
+    console.log(small1)
+}
+
+someFunction(populations)
+
+let category = {id:1, name1:"İçecek"}
+console.log(category.id)
+console.log(category["name1"])
+
+let {id,name1} = category
+console.log(id)
+console.log(name1)
